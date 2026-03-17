532f2c54372202bf4da6df62e4a1f838422fcbb0
Thaw Linn Oo <thawlinnoo7@gmail.com>
2026-03-17 16:24:04 +0700

    Day 1

---
+++ Day1.py
@@ -0,0 +1,14 @@
+# \n makes it print in new line
+# print("hello world\nhello world")
+
+# use + to combine string (string concatenation)
+# print("Hello" + " Marcus")
+
+# asking input
+# a = input("text to display the user: ")
+# print("hello " + a + "!")
+
+print("Welcome to the Brand Name Generator")
+city = input("What is the name of the city you grew up in?\n")
+pet = input("What is your pet's name?\n")
+print("Your brand name could be " + city + " " + pet)
