35c14444a9411939831daa5f5eb6c43b217e1be6
Thaw Linn Oo <thawlinnoo7@gmail.com>
2026-05-20 18:53:12 +0700

    Day-54

---
+++ Day-54/hello.py
@@ -0,0 +1,50 @@
+# from flask import Flask
+
+# app = Flask(__name__)
+
+# @app.route("/") #using decorator function here "@"
+# def hello():
+#     return "Hello, Flask!"
+
+# @app.route("/Bye")
+# def bye():
+#     return "Bye"
+
+
+# if __name__ == "__main__":
+#     app.run()
+# #---------------------
+
+
+##decorator function
+import time
+current_time = time.time()
+print(current_time) # seconds since Jan 1st, 1970 
+
+
+
+def speed_calc_decorator(function):
+    def execute_time_check():
+        function()
+        after_run_time = time.time()
+        print(f"{function} run speed: {after_run_time-current_time}")
+    return execute_time_check
+      
+@speed_calc_decorator
+def fast_function():
+    
+    for i in range(1000000):
+        i * i
+
+        
+@speed_calc_decorator
+def slow_function():
+    
+    for i in range(10000000):
+        i * i
+        
+        
+result = speed_calc_decorator(fast_function)
+result()
+result = speed_calc_decorator(slow_function)
+result()
